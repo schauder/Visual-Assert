09e1f3e1e54a2e6ab7fc4082fae6db73a4734bde
Jens Schauder <jens.schauder@freenet.de>
2011-01-05 17:28:12 +0100

    added dependency for logging and removed experimental class.

--- project/build/visualAssert.scala
+++ project/build/visualAssert.scala
@@ -2,5 +2,6 @@ import sbt._
 
 class VisualAssertProject(info: ProjectInfo) extends DefaultProject(info)
 {
-  val dep = "org.scalatest" % "scalatest" % "1.2"
+  val scalatest = "org.scalatest" % "scalatest" % "1.2"
+  val grizzled = "org.clapper" %% "grizzled-slf4j" % "0.3.2"
 }
